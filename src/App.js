4ab0a395a7a8d005e1582f6ec77b3e9dc71abd2c
MatthewSutton85 <sutto85@gmail.com>
2021-04-06 13:03:19 +1000

    made changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Matthew Sutton
+        </h1>
+        <h3>
+          Portfolio
+        </h3>
       </header>
     </div>
   );
