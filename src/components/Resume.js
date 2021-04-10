3928a73319491516a7d80cec37b9ede5a41179b8
MatthewSutton85 <sutto85@gmail.com>
2021-04-10 19:04:44 +1000

    updates

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Tab from 'react-bootstrap/Tab';
 
 class Resume extends Component {
   render() {
@@ -11,26 +13,64 @@ class Resume extends Component {
           <Row>
             <Col><h2>Resume</h2></Col>
           </Row>
+          <Tab.Container className="list-group" defaultActiveKey="#first">
           <Row>
             <Col>
               <h4>Education</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
-                  Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+              <ListGroup>
+                <ListGroup.Item action href="#first">
+                  <p>Diploma of Software Development</p>
+                </ListGroup.Item>
+                <ListGroup.Item action href="#second">
+                  <p>Diploma of Web Development</p>
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
             <Col>
-              <h4>Experience</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
-                Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
-            </Col>
-            
-          </Row>
+              <Tab.Content>
+                <Tab.Pane eventKey="#first">
+                  <h4>Diploma of Software Development</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
+                  Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="#second">
+                  <h4>Diploma of Web Development</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
+                  Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+                </Tab.Pane>
+              </Tab.Content>              
+            </Col>            
+            </Row>
+            </Tab.Container>  
+            <Tab.Container className="list-group" defaultActiveKey="#UTG">
           <Row>
             <Col>
-              <h4>Skills</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
-                Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+              <h4>Experience</h4>
+              <ListGroup>
+                <ListGroup.Item action href="#UTG">
+                  <p>Unlock The Game</p>
+                </ListGroup.Item>
+                <ListGroup.Item action href="#freelance">
+                  <p>Freelance</p>
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
-          </Row>
+            <Col>
+              <Tab.Content>
+                <Tab.Pane eventKey="#UTG">
+                  <h4>Unlock The Game</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
+                  Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="#freelance">
+                  <h4>Freelance</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisi ante, auctor id elit ullamcorper, placerat auctor massa. Sed vel tempus massa. 
+                  Etiam eget orci id ligula dapibus semper vitae at arcu. Aliquam sagittis pretium orci. Vestibulum mattis odio nec massa vestibulum, et euismod lorem scelerisque.</p>
+                </Tab.Pane>
+              </Tab.Content>              
+            </Col>            
+            </Row>
+            </Tab.Container>
         </Container>
     );
   }
